b0da4be889bfb7235f4f69589f063d2fa5320c10
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2014-03-25 12:47:40 +0200

    Update gsui_functions.sh

--- centos/gsui_functions.sh
+++ centos/gsui_functions.sh
@@ -4,7 +4,7 @@ export GSUI_FUNCTION_ROOT="https://raw.githubusercontent.com/guy-mograbi-at-giga
 run_gsui_function_script( ){
 
     NAME = $1
-    wget --no-check-certificate "$GSUI_FUNCTION_ROOT/$NAME.sh" -O $NAME.sh
+    wget --no-check-certificate "$GSUI_FUNCTION_ROOT/$1.sh" -O $1.sh
     chmod +x $NAME.sh
     source $NAME.sh
 }
