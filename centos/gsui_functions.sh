ec0545b867910d68ef51559f05eafdd28f3fcb7e
guym <guym@gigaspaces.com>
2014-03-25 15:25:18 +0200

    adding "render_template"

--- centos/gsui_functions.sh
+++ centos/gsui_functions.sh
@@ -1,10 +1,11 @@
-export GSUI_FUNCTION_ROOT="https://raw.githubusercontent.com/guy-mograbi-at-gigaspaces/gs-ui-automation-tools/master/centos"
-
 
 run_gsui_function_script( ){
 
     NAME = $1
-    wget --no-check-certificate "$GSUI_FUNCTION_ROOT/$NAME.sh" -O $NAME.sh
+    if [ ! -f $NAME.sh ];then
+        echo "missing file $NAME.sh"
+        exit 1
+    fi
     chmod +x $NAME.sh
     source $NAME.sh
 }
