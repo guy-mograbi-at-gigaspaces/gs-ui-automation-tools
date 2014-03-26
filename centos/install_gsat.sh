99d71d88569177863230e8a7c28d9f80bd16bee2
guym <guym@gigaspaces.com>
2014-03-26 22:17:39 +0200

    adding "latest" version.

--- centos/install_gsat.sh
+++ centos/install_gsat.sh
@@ -3,17 +3,20 @@
 # and then use by running
 # source /opt/gsat/gsui_function.sh
 
-if [ ! -f /opt/gsat/gsui_functions.sh ]; then
-    mkdir /opt/gsat
-    echo "download gsui_functions.sh"
-    wget --no-cache --no-check-certificate -O /opt/gsat/gsat.tar http://get.gsdev.info/gsat/1.0.0/gsat-1.0.0.tar
-    tar -xvf /opt/gsat/gsat.tar -C /opt/gsat
-    echo "loading gsat functions"
-    dos2unix *.sh /opt/gsat/*.sh
-    chmod +x *.sh /opt/gsat/*.sh
-else
-    echo "gsat already installed"
+echo "installing gsat"
+
+if [ -e /opt/gsat ];then
+    echo "deleting old version"
+    rm -Rf /opt/gsat
 fi
+
+mkdir /opt/gsat
+echo "download gsui_functions.sh"
+wget --no-cache --no-check-certificate -O /opt/gsat/gsat.tar http://get.gsdev.info/gsat/1.0.0/gsat-1.0.0.tar
+tar -xvf /opt/gsat/gsat.tar -C /opt/gsat
+echo "loading gsat functions"
+dos2unix *.sh /opt/gsat/*.sh
+chmod +x *.sh /opt/gsat/*.sh
 
 
 
