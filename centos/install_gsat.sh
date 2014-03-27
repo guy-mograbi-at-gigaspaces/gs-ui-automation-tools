7fc6127d2f5934b16a6d4aa2b270c234685d368d
Evgeny Fisher <evgenyf@gigaspaces.com>
2014-03-27 13:28:45 +0200

    fixed chmod

--- centos/install_gsat.sh
+++ centos/install_gsat.sh
@@ -15,8 +15,8 @@ echo "download gsui_functions.sh"
 wget --no-cache --no-check-certificate -O /opt/gsat/gsat.tar http://get.gsdev.info/gsat/1.0.0/gsat-1.0.0.tar
 tar -xvf /opt/gsat/gsat.tar -C /opt/gsat
 echo "loading gsat functions"
-dos2unix *.sh /opt/gsat/*.sh
-chmod +x *.sh /opt/gsat/*.sh
+dos2unix  /opt/gsat/*.sh
+chmod +x  /opt/gsat/*.sh
 source /opt/gsat/gsui_functions.sh
 
 
