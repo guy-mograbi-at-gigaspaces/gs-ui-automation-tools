52deaf673a795fc814ba37e68bd3a0c3113dd76c
guym <guym@gigaspaces.com>
2014-03-29 15:12:17 +0200

    moving "create" table into here. gsat will be incharge of your DB versioning from beginning to end.

--- centos/install_mongo.sh
+++ centos/install_mongo.sh
@@ -1,7 +1,7 @@
 if [ -e /etc/init.d/mongod ]; then
     echo "mongo already installed... skipping"
 else
-    cp -f /opt/gsat/mongo.repo /etc/yum.repos.d/mogno.repo
+    \cp -f /opt/gsat/mongo.repo /etc/yum.repos.d/mogno.repo
     yum clean all
     yum -y install mongo-10gen mongo-10gen-server
 fi
