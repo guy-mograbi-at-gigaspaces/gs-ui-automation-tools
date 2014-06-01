c92f13a2d53df91653a80726fc63c5d74e27850d
guym <guym@gigaspaces.com>
2014-06-01 18:40:18 +0300

    skipping if install_mysql is not on localhost. not failing as we want to be able to seamlessly support local and remote configuration without special script handling.

--- centos/install_mysql.sh
+++ centos/install_mysql.sh
@@ -1,35 +1,40 @@
 #install mysql
 
-if [ -e /etc/init.d/mysqld ]; then
-    echo "mysql already installed, skipping"
+
+if [ "$DB_HOST" = "localhost" ]; then
+    echo "cannot install mysql on remote host $DB_HOST"
 else
-    CURRENT_DIRECTORY=`pwd`
+    echo "installing mysql on localhost"
+    if [ -e /etc/init.d/mysqld ]; then
+        echo "mysql already installed, skipping"
+    else
+        CURRENT_DIRECTORY=`pwd`
 
-    cd "$(dirname "$0")"
+        cd "$(dirname "$0")"
 
-    check_exists DB_ADMIN
-    check_exists DB_ADMIN_PASSWORD
-
-
-    echo "installing mysql"
-    yum -y install mysql-server mysql php-mysql
-    chkconfig --levels 235 mysqld on
-    service mysqld start
-
-    echo "configuring DB_ADMIN on mysql. ignoring errors by adding || true for each line"
-
-    mysql -u $DB_ADMIN -e "SET PASSWORD FOR '$DB_ADMIN'@'localhost' = PASSWORD('$DB_ADMIN_PASSWORD');" || true
-    mysql -u $DB_ADMIN -p$DB_ADMIN_PASSWORD -e "SET PASSWORD FOR '$DB_ADMIN'@'127.0.0.1' = PASSWORD('$DB_ADMIN_PASSWORD');" || true
-    mysql -u $DB_ADMIN -e "SET PASSWORD FOR '$DB_ADMIN'@'127.0.0.1' = PASSWORD('$DB_ADMIN_PASSWORD');" || true
-    mysql -u $DB_ADMIN -p$DB_ADMIN_PASSWORD -e "SET PASSWORD FOR '$DB_ADMIN'@'localhost.localdomain' = PASSWORD('$DB_ADMIN_PASSWORD');" || true
-    mysql -u $DB_ADMIN -e "SET PASSWORD FOR '$DB_ADMIN'@'localhost.localdomain' = PASSWORD('$DB_ADMIN_PASSWORD');" || true
-    mysql -u $DB_ADMIN -p$DB_ADMIN_PASSWORD -e "DROP USER ''@'localhost';" || true
-    mysql -u $DB_ADMIN -e "DROP USER ''@'localhost';" || true
-    mysql -u $DB_ADMIN -p$DB_ADMIN_PASSWORD  -e  "DROP USER ''@'localhost.localdomain';" || true
-    mysql -u $DB_ADMIN -e "DROP USER ''@'localhost.localdomain';" || true
+        check_exists DB_ADMIN
+        check_exists DB_ADMIN_PASSWORD
 
 
+        echo "installing mysql"
+        yum -y install mysql-server mysql php-mysql
+        chkconfig --levels 235 mysqld on
+        service mysqld start
 
-    cd $CURRENT_DIRECTORY
+        echo "configuring DB_ADMIN on mysql. ignoring errors by adding || true for each line"
+
+        mysql -u $DB_ADMIN -e "SET PASSWORD FOR '$DB_ADMIN'@'localhost' = PASSWORD('$DB_ADMIN_PASSWORD');" || true
+        mysql -u $DB_ADMIN -p$DB_ADMIN_PASSWORD -e "SET PASSWORD FOR '$DB_ADMIN'@'127.0.0.1' = PASSWORD('$DB_ADMIN_PASSWORD');" || true
+        mysql -u $DB_ADMIN -e "SET PASSWORD FOR '$DB_ADMIN'@'127.0.0.1' = PASSWORD('$DB_ADMIN_PASSWORD');" || true
+        mysql -u $DB_ADMIN -p$DB_ADMIN_PASSWORD -e "SET PASSWORD FOR '$DB_ADMIN'@'localhost.localdomain' = PASSWORD('$DB_ADMIN_PASSWORD');" || true
+        mysql -u $DB_ADMIN -e "SET PASSWORD FOR '$DB_ADMIN'@'localhost.localdomain' = PASSWORD('$DB_ADMIN_PASSWORD');" || true
+        mysql -u $DB_ADMIN -p$DB_ADMIN_PASSWORD -e "DROP USER ''@'localhost';" || true
+        mysql -u $DB_ADMIN -e "DROP USER ''@'localhost';" || true
+        mysql -u $DB_ADMIN -p$DB_ADMIN_PASSWORD  -e  "DROP USER ''@'localhost.localdomain';" || true
+        mysql -u $DB_ADMIN -e "DROP USER ''@'localhost.localdomain';" || true
+
+
+
+        cd $CURRENT_DIRECTORY
+    fi
 fi
-
